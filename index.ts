744c77ae620875de57d4f56b09dc6e12d5aa6fbe
Elliott Lawrence <elliottlawrence7@gmail.com>
2024-06-16 12:55:14 -0700

    Update package.json

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { IEventSource, SSE, SSEParser } from "./lib/sse-parser";
+import { SSEParserStream } from "./lib/sse-parser-stream";
+
+export { SSEParser, SSEParserStream };
+export type { SSE, IEventSource };
--- lib/main.ts
+++
@@ -1,5 +0,0 @@
-import { IEventSource, SSE, SSEParser } from "./sse-parser";
-import { SSEParserStream } from "./sse-parser-stream";
-
-export { SSEParser, SSEParserStream };
-export type { SSE, IEventSource };
